007be3787b6507797aaac022b6906c651556a425
Jaid <jaid.jsx@gmail.com>
2019-05-20 07:55:45 +0200

    Added code to setupJaid
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- setupJaid.bash
+++ setupJaid.bash
@@ -1,7 +1,11 @@
+#!/usr/bin/env bash
+set -e
 set -o errexit
 
 mkdir -p ~/bin
 mkdir -p ~/src
 
-downloadJaidScript installZsh
-installZsh
+downloadJaidScript setupLanguage && $_
+downloadJaidScript installZsh && $_
+downloadJaidScript installNode && $_
+downloadJaidScript installBat && $_
