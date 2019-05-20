09c40e3ead4ed7bab7e4521035d72679c07e540c
Jaid <jaid.jsx@gmail.com>
2019-05-20 11:39:06 +0200

    Fixed chown
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- commands/setupUser.bash
+++ commands/setupUser.bash
@@ -5,6 +5,6 @@ set -o errexit
 targetUser=jaid
 bashFile=/home/$targetUser/setup.bash
 downloadFile "https://raw.githubusercontent.com/Jaid/setup-server/master/setupJaid.bash" $bashFile
-chown $targetUser $bashFile
+chown --recursive $targetUser:$targetUser /home/$targetUser
 chmod +x $bashFile
 bash -c "sudo --non-interactive --set-home --user=$targetUser DEBIAN_FRONTEND=noninteractive PS4='[:\$LINENO] ' zsh -e -x $bashFile > /home/$targetUser/setup.log 2>&1"
