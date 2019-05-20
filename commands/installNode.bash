7f421aaaef7e1b6afc99b542b43949e74cbe1b9c
Jaid <jaid.jsx@gmail.com>
2019-05-20 07:47:50 +0200

    Added global packages to npm
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- commands/installNode.bash
+++ commands/installNode.bash
@@ -2,9 +2,8 @@
 set -e
 set -o errexit
 
-#yarn global add n
-#sudo "$(yarn global bin)/n" stable --quiet # Install latest stable NodeJS via n
-
 executeDownload "https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh"
 nvm install node
 nvm use node
+
+npm install --global date-now-cli open-cli pover replace-in-file rimraf yarn
