2b6319e79e48837dab5b8daf35365bd61de4e68c
Jaid <jaid.jsx@gmail.com>
2019-05-20 04:30:32 +0200

    Fixed setupUser
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- commands/setupUser.bash
+++ commands/setupUser.bash
@@ -5,4 +5,4 @@ bashFile=/home/$user/setup.bash
 downloadFile "https://raw.githubusercontent.com/Jaid/setup-server/master/setupJaid.bash" $bashFile
 chown $user $bashFile
 chmod +x $bashFile
-nohup bash -c "sudo --non-interactive --set-home --user=$user DEBIAN_FRONTEND=noninteractive PS4='[:\$LINENO] ' bash -e -x /home/$user/src/setup/setup.bash > /home/$user/src/setup/setup.log 2>&1"
+nohup bash -c "sudo --non-interactive --set-home --user=$user DEBIAN_FRONTEND=noninteractive PS4='[:\$LINENO] ' bash -e -x $bashFile > /home/$user/setup.log 2>&1"
