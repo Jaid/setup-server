c9b90a657d14e413bce2eb949de4e44bbd1d03c1
Jaid <jaid.jsx@gmail.com>
2019-05-20 07:53:22 +0200

    Added installBat
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

---
+++ commands/installBat.bash
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -e
+set -o errexit
+
+batVersion="0.11.0"
+
+SRC_DIR=~/src
+mkdir --parents $SRC_DIR/bat
+cd $_
+download "https://github.com/sharkdp/bat/releases/download/v$batVersion/bat_$batVersion_amd64.deb" release.deb
+sudo dpkg -i release.deb
+cd
+rm -rf $SRC_DIR/bat
