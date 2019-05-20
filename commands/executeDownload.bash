f1968f6fbc6c193fc7ab0f076626ee8b5a74c652
Jaid <jaid.jsx@gmail.com>
2019-05-20 03:59:30 +0200

    Trying out possible fixes
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- commands/executeDownload.bash
+++ commands/executeDownload.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-mkdir -p ~/.cache/executeDownload
+mkdir -p "~/.cache/executeDownload"
 hash=$(echo -n "$1" | md5sum | awk '{print $1}')
 downloadPath="~/.cache/executeDownload/$hash.bash"
 downloadFile "$1" "$downloadPath"
