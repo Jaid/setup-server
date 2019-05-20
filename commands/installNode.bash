ec46359261e391d554f2ba3b0b9ade7b5aa2abc8
Jaid <jaid.jsx@gmail.com>
2019-05-20 08:14:00 +0200

    Added json5 node package
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- commands/installNode.bash
+++ commands/installNode.bash
@@ -6,4 +6,4 @@ executeDownload "https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh"
 nvm install node
 nvm use node
 
-npm install --global date-now-cli open-cli pover replace-in-file rimraf yarn
+npm install --global date-now-cli open-cli pover replace-in-file rimraf yarn json5
