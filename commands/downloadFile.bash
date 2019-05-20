6c49a00a47c79b7287f96cc15b32c87e69e262dc
Jaid <jaid.jsx@gmail.com>
2019-05-20 12:20:16 +0200

    Trying another chown usage
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- commands/downloadFile.bash
+++ commands/downloadFile.bash
@@ -3,4 +3,4 @@ set -e
 set -o errexit
 
 curl --fail --silent --header 'Cache-Control: no-cache' --location --retry 3 "$1" | sudo dd of="$2"
-sudo chown $USER "$2"
+sudo chown $(whoami) "$2"
--- commands/setupUser.bash
+++ commands/setupUser.bash
@@ -5,6 +5,6 @@ set -o errexit
 targetUser=jaid
 bashFile=/home/$targetUser/setup.bash
 downloadFile "https://raw.githubusercontent.com/Jaid/setup-server/master/setupJaid.bash" $bashFile
-#chown --recursive $targetUser:$targetUser /home/$targetUser Fails for some reason
+chown --recursive $targetUser:$targetUser /home/$targetUser
 chmod +x $bashFile
 bash -c "sudo --non-interactive --set-home --user=$targetUser DEBIAN_FRONTEND=noninteractive zsh -e -x $bashFile > /home/$targetUser/setup.log 2>&1"
