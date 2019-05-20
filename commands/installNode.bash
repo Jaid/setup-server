d56167550fe0ff66a57caeef85bd3b33b38ea9f4
Jaid <jaid.jsx@gmail.com>
2019-05-20 03:19:43 +0200

    Updated installNode
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- commands/installNode.bash
+++ commands/installNode.bash
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
-yarn global add n
-sudo "$(yarn global bin)/n" stable --quiet # Install latest stable NodeJS via n
+#yarn global add n
+#sudo "$(yarn global bin)/n" stable --quiet # Install latest stable NodeJS via n
+
+executeDownloaded https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh
+nvm install node
+nvm use node
