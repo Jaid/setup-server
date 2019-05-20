49303fcfac85f5ff115659a344bc5a3e8bbff52e
Jaid <jaid.jsx@gmail.com>
2019-05-20 13:46:12 +0200

    Fixed nvm.sh reference
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- commands/installNode.bash
+++ commands/installNode.bash
@@ -3,7 +3,7 @@ set -e
 set -o errexit
 
 gitClone nvm-sh/nvm
-source ~/src/nvm.sh
+source ~/src/nvm/nvm.sh
 nvm install node
 
 npm install --global date-now-cli open-cli pover replace-in-file rimraf yarn json5
