c9461661c2387a87434ab97093bc12e8232617f6
Jaid <jaid.jsx@gmail.com>
2019-05-20 13:50:30 +0200

    Fixed installBat
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- commands/installBat.bash
+++ commands/installBat.bash
@@ -7,7 +7,7 @@ batVersion="0.11.0"
 SRC_DIR=~/src
 mkdir --parents $SRC_DIR/bat
 cd $_
-download "https://github.com/sharkdp/bat/releases/download/v$batVersion/bat_$batVersion_amd64.deb" release.deb
+downloadFile "https://github.com/sharkdp/bat/releases/download/v$batVersion/bat_${batVersion}_amd64.deb" release.deb
 sudo dpkg -i release.deb
 cd
 rm -rf $SRC_DIR/bat
