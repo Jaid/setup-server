bdd9648c5807c8f90367a8fff375ae0f284ecdc3
Jaid <jaid.jsx@gmail.com>
2019-05-20 04:23:25 +0200

    Added setupJaid
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

---
+++ commands/setupUser.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+user=jaid
+bashFile=/home/$user/setup.bash
+downloadFile "https://raw.githubusercontent.com/Jaid/setup-server/master/setupJaid.bash" $bashFile
+chown $user $bashFile
+chmod +x $bashFile
+nohup bash -c "sudo --non-interactive --set-home --user=$user DEBIAN_FRONTEND=noninteractive PS4='[:\$LINENO] ' bash -e -x /home/$user/src/setup/setup.bash > /home/$user/src/setup/setup.log 2>&1"
