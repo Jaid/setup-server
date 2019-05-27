c11ac1f673ad5a72d057ddc3309d816718f90455
Jaid <jaid.jsx@gmail.com>
2019-05-27 17:57:34 +0200

    Fixed `setupJaid`
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- setupJaid.bash
+++ setupJaid.bash
@@ -7,7 +7,7 @@ mkdir -p ~/src
 
 downloadJaidScript setupLanguage && $_
 downloadJaidScript installZsh && $_
-downloadJaidScript installNode && $_
+downloadJaidScript installNode && $_ && source $HOME/src/nvm/nvm.sh
 downloadJaidScript installBat && $_
 downloadJaidScript installFfmpeg && $_
 downloadJaidScript installJaidbot && $_ && jaidbot
