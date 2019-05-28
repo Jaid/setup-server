a590244697a542c12522233a4b753620b6b56a94
Jaid <jaid.jsx@gmail.com>
2019-05-28 23:53:17 +0200

    Added `installYoutubedl`
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

---
+++ commands/installYoutubedl.bash
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+set -o errexit
+
+tag=$(latest-github-release --owner ytdl-org --repository youtube-dl --tag | tr -d '\r\n')
+downloadFile=/usr/local/bin/youtube-dl
+sudo downloadFile "https://github.com/ytdl-org/youtube-dl/releases/download/2019.05.11/youtube-dl" $downloadFile
+sudo chmod +x $downloadFile
+
+echo -e "#\!/bin/sh\n/usr/local/bin/youtube-dl --update" | sudo dd of=/etc/init.d/updateYoutubedl
+sudo chmod +x /etc/init.d/updateYoutubedl
