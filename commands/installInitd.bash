adaefda359647a1e6d2e0d3e77c3cd7a7f70bed6
Jaid <jaid.jsx@gmail.com>
2019-05-29 22:09:27 +0200

    Added init.d scripts
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

---
+++ commands/installInitd.bash
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -e
+set -o errexit
+
+downloadFile=/etc/init.d/$1
+downloadFile https://raw.githubusercontent.com/Jaid/setup-server/master/init.d/$1 $downloadFile
+sudo chmod +x $downloadFile
--- commands/installYoutubedl.bash
+++ commands/installYoutubedl.bash
@@ -7,6 +7,4 @@ downloadFile=/usr/local/bin/youtube-dl
 sudo downloadFile "https://github.com/ytdl-org/youtube-dl/releases/download/$tag/youtube-dl" $downloadFile
 sudo chmod +x $downloadFile
 
-initScript=/etc/init.d/updateYoutubeDl
-echo -e "#\!/bin/sh\n/usr/local/bin/youtube-dl --update" | sudo dd of=$initScript
-sudo chmod +x $initScript
+sudo installInitd updateYoutubeDl
