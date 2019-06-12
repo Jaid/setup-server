e7ef861f1ed332005b6cbefc23c17ebf015d3497
Jaid <jaid.jsx@gmail.com>
2019-06-12 23:51:24 +0200

    Added npm CLI package
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- commands/installNode.bash
+++ commands/installNode.bash
@@ -3,4 +3,4 @@ set -e
 set -o errexit
 
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-sudo npm install --global date-now-cli open-cli pover replace-in-file rimraf yarn json5 latest-github-release getfilesize-cli pkg
+sudo npm install --global date-now-cli open-cli pover replace-in-file rimraf yarn json5 latest-github-release getfilesize-cli pkg bin-version-cli
