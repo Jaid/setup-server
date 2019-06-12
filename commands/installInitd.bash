293b46f75ed3fc881088c0c2de01d23866a5b82b
Jaid <jaid.jsx@gmail.com>
2019-06-13 00:22:55 +0200

    Migrated init scripts to services
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- commands/installInitd.bash
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-set -o errexit
-
-downloadFile=/etc/init.d/$1
-downloadFile https://raw.githubusercontent.com/Jaid/setup-server/master/init.d/$1 $downloadFile
-sudo chmod +x $downloadFile
-
-echo Added $downloadFile
--- commands/installYoutubeDl.bash
+++ commands/installYoutubeDl.bash
@@ -6,5 +6,3 @@ tag=$(latest-github-release --owner ytdl-org --repository youtube-dl --tag | tr 
 downloadFile=/usr/local/bin/youtube-dl
 sudo downloadFile "https://github.com/ytdl-org/youtube-dl/releases/download/$tag/youtube-dl" $downloadFile
 sudo chmod +x $downloadFile
-
-sudo installInitd updateYoutubeDl
