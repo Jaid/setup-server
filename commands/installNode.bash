8aa2b3fded99e71c386883f8aca3b82b1322b7bc
Jaid <jaid.jsx@gmail.com>
2019-06-15 05:19:55 +0200

    Added `updateNpmPackages`
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- commands/installNode.bash
+++ commands/installNode.bash
@@ -3,4 +3,6 @@ set -e
 set -o errexit
 
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-sudo npm install --global date-now-cli open-cli pover replace-in-file rimraf yarn json5 getfilesize-cli pkg bin-version-cli
+sudo npm install --global date-now-cli open-cli pover replace rimraf yarn json5 getfilesize-cli pkg bin-version-cli npm-check-updates
+
+installService updateNpmPackages
