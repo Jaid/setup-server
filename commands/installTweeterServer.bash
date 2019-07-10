9f56cc170b34bc66883315623bd6e743096a48bc
Jaid <jaid.jsx@gmail.com>
2019-07-11 01:04:50 +0200

    Added support for `tweeter-server`
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

---
+++ commands/installTweeterServer.bash
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -e
+set -o errexit
+
+latestTag=$(getLatestRepoVersion jaid/tweeter-server | tr -d '\r\n')
+downloadUrl=https://github.com/Jaid/tweeter-server/releases/download/$latestTag/tweeter-server_${latestTag}_amd64.deb
+downloadFile=/tmp/tweeter-server.deb
+
+downloadFile "$downloadUrl" $downloadFile
+sudo dpkg --install $downloadFile
+sudo aptGet install --fix-broken
+
+sudo ufw allow 18888
+sudo ufw allow 18889
+
+if [ ! -f /etc/systemd/system/tweeterServer.service ]; then
+  sudo installService tweeterServer
+fi
