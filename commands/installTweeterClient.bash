6862a0ba3d79ec6043f6cf257251f80ee10ed44b
Jaid <jaid.jsx@gmail.com>
2019-07-14 04:52:30 +0200

    Changed `tweeter-client` and `tweeter-server` includings
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- commands/installTweeterClient.bash
+++ commands/installTweeterClient.bash
@@ -2,14 +2,6 @@
 set -e
 set -o errexit
 
-latestTag=$(getLatestRepoVersion jaid/tweeter-client | tr -d '\r\n')
-downloadUrl=https://github.com/Jaid/tweeter-client/releases/download/$latestTag/tweeter-client_${latestTag}_amd64.deb
-downloadFile=/tmp/tweeter-client.deb
-
-downloadFile "$downloadUrl" $downloadFile
-sudo dpkg --install $downloadFile
-sudo aptGet install --fix-broken
-
 sudo ufw allow 18888
 sudo ufw allow 18889
 
--- commands/installTweeterServer.bash
+++ commands/installTweeterServer.bash
@@ -2,14 +2,6 @@
 set -e
 set -o errexit
 
-latestTag=$(getLatestRepoVersion jaid/tweeter-server | tr -d '\r\n')
-downloadUrl=https://github.com/Jaid/tweeter-server/releases/download/$latestTag/tweeter-server_${latestTag}_amd64.deb
-downloadFile=/tmp/tweeter-server.deb
-
-downloadFile "$downloadUrl" $downloadFile
-sudo dpkg --install $downloadFile
-sudo aptGet install --fix-broken
-
 sudo ufw allow 18888
 sudo ufw allow 18889
 
