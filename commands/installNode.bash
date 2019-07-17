36957e7ff5050091ebb6e989088884d0adf78857
Jaid <jaid.jsx@gmail.com>
2019-07-17 21:38:17 +0200

    Fixed Node installation
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- commands/installNode.bash
+++ commands/installNode.bash
@@ -2,6 +2,7 @@
 set -e
 set -o errexit
 
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 aptGet install nodejs
 sudo npm install --global npm date-now-cli open-cli pover replace rimraf yarn json5 getfilesize-cli pkg bin-version-cli npm-check-updates find-by-extension-cli package-name-cli package-field-cli surge foreach-cli
 
