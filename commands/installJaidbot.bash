911d30e531d6afb2e1cfb8a1ae0499b00dce5a81
Jaid <jaid.jsx@gmail.com>
2019-07-24 07:59:35 +0200

    Replaced jaidbot.deb with npm package jaidbot
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- commands/installJaidbot.bash
+++ commands/installJaidbot.bash
@@ -2,17 +2,9 @@
 set -e
 set -o errexit
 
-latestTag=$(getLatestRepoVersion jaid/jaidbot | tr -d '\r\n')
-downloadUrl=https://github.com/Jaid/jaidbot/releases/download/$latestTag/jaidbot_${latestTag}_amd64.deb
-downloadFile=/tmp/jaidbot.deb
-
-downloadFile "$downloadUrl" $downloadFile
-sudo dpkg --install $downloadFile
-sudo aptGet install --fix-broken
-
 sudo ufw allow 9411
 
-mkdir -p /home/jaid/.config/Jaidbot
+mkdir -p "/home/jaid/.config/Jaidbot"
 
 if [ ! -f /etc/systemd/system/jaidbot.service ]; then
   sudo installService jaidbot
