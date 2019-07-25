5965c7ec47c3658c1d39bfc9a1eab118e75d7391
Jaid <jaid.jsx@gmail.com>
2019-07-25 12:13:46 +0200

    Added global node package `jaid-logger-tail`
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- commands/installNode.bash
+++ commands/installNode.bash
@@ -4,6 +4,6 @@ set -o errexit
 
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 aptGet install nodejs
-sudo npm install --global npm date-now-cli open-cli pover replace rimraf yarn json5 getfilesize-cli pkg bin-version-cli npm-check-updates find-by-extension-cli package-name-cli package-field-cli surge foreach-cli
+sudo npm install --global npm date-now-cli open-cli pover replace rimraf yarn json5 getfilesize-cli pkg bin-version-cli npm-check-updates find-by-extension-cli package-name-cli package-field-cli surge foreach-cli jaid-logger-tail
 
 installService updateNpmPackages
