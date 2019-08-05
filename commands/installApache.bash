60e0306bca055537d1361feebfbb9e93b9d4db82
Jaid <jaid.jsx@gmail.com>
2019-08-05 06:29:54 +0200

    Added missing SSL parts
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- commands/installApache.bash
+++ commands/installApache.bash
@@ -7,7 +7,9 @@ aptGet install apache2
 sudo ufw allow 'OpenSSH'
 sudo ufw allow 'Apache Secure'
 
+sudo a2enmod ssl
 sudo a2enmod http2
+
 echo "Protocols h2 http/1.1" | sudo tee --append /etc/apache2/apache2.conf
 
 sudo service apache2 restart
