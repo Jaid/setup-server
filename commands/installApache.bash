4970b47ec3c3bc2995812ea0dc1a0eecb3e3d671
Jaid <jaid.jsx@gmail.com>
2019-08-06 03:22:36 +0200

    Added some Apache mods
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- commands/installApache.bash
+++ commands/installApache.bash
@@ -10,6 +10,12 @@ sudo ufw allow 'Apache Secure'
 sudo a2enmod ssl
 sudo a2enmod http2
 sudo a2enmod rewrite
+sudo a2enmod proxy
+sudo a2enmod proxy_http
+sudo a2enmod proxy_http2
+sudo a2enmod proxy_wstunnel
+sudo a2enmod headers
+sudo a2enmod brotli
 
 echo "Protocols h2 http/1.1" | sudo tee --append /etc/apache2/apache2.conf
 
