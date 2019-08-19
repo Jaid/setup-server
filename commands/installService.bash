cef3d52a2e23c96c506cfb584abe8b1fe66913f0
Jaid <jaid.jsx@gmail.com>
2019-08-19 12:57:41 +0200

    Improved installService
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- commands/installService.bash
+++ commands/installService.bash
@@ -4,13 +4,18 @@ set -o errexit
 
 downloadFile=/etc/systemd/system/$1.service
 downloadFile https://raw.githubusercontent.com/Jaid/setup-server/master/services/$1.service $downloadFile
-sudo systemctl daemon-reload
-sudo systemctl enable $1.service
-sudo service $1 restart
 
-timerStatusCode==$(curl --head https://raw.githubusercontent.com/Jaid/setup-server/master/timers/$1.timer --write-out "%{http_code}" --output /dev/null --silent)
-if [ $timerStatusCode -ne "404" ]; then
+timerStatusCode=$(curl --head https://raw.githubusercontent.com/Jaid/setup-server/master/timers/$1.timer --write-out "%{http_code}\n" --output /dev/null --silent)
+if [ $timerStatusCode -eq 200 ]; then
   echo "Found $1.timer"
+  timerDownloadFile=/etc/systemd/system/$1.timer
+  downloadFile https://raw.githubusercontent.com/Jaid/setup-server/master/timers/$1.timer $timerDownloadFile
+  sudo systemctl daemon-reload
+  sudo systemctl enable $1.timer
+else
+  sudo systemctl daemon-reload
+  sudo systemctl enable $1.service
+  sudo service $1 restart
 fi
 
 echo Added and started $downloadFile
