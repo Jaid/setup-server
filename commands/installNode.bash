121170fc8bda967dd01f53a8b946b4c7e48d9085
Jaid <jaid.jsx@gmail.com>
2020-03-16 07:33:28 +0100

    Updated Node version to 13
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- commands/installNode.bash
+++ commands/installNode.bash
@@ -2,7 +2,7 @@
 set -e
 set -o errexit
 
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
 aptGet install nodejs
 sudo npm install --global --unsafe-perm npm date-now-cli open-cli pover replace rimraf yarn json5 getfilesize-cli pkg bin-version-cli npm-check-updates find-by-extension-cli package-name-cli package-field-cli surge foreach-cli jaid-logger-tail
 
