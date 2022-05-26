fc59f3bd17ca844ba007030957312d6b89277693
Jaid <jaid.jsx@gmail.com>
2022-05-26 21:07:34 +0200

    Fixed netdata
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- commands/installNetdata.bash
+++ commands/installNetdata.bash
@@ -2,12 +2,10 @@
 set -e
 set -o errexit
 
-installCompose github-release-writer
+installCompose netdata
 
-cd ~/docker/github-release-writer
+cd ~/docker/netdata
 
-mkdir config
 docker-compose up
-docker-compose down
 
 cd ~
