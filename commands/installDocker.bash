0e606c57757bef85c7cb43fc0c09e9011593436c
Jaid <jaid.jsx@gmail.com>
2022-05-26 20:18:37 +0200

    Changed docker to rootless
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- commands/installDocker.bash
+++ commands/installDocker.bash
@@ -5,7 +5,7 @@ set -o errexit
 mkdir -p ~/docker
 cd ~/docker
 
-curl -fsSL https://get.docker.com -o get-docker.sh
+curl -fsSL https://get.docker.com/rootless -o get-docker.sh
 bash get-docker.sh
 rm get-docker.sh
 
