52ca6f1497aa9c6b2011073aa7767680115b1172
Jaid <jaid.jsx@gmail.com>
2022-05-26 10:27:35 +0200

    Debugging
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- setupJaid.bash
+++ setupJaid.bash
@@ -9,12 +9,12 @@ downloadJaidScript setupLanguage && $_
 downloadJaidScript installZsh && $_
 downloadJaidScript installNode && $_
 downloadJaidScript installBat && $_
-downloadJaidScript installFfmpeg && $_
-downloadJaidScript installYoutubeDl && $_
-downloadJaidScript installDocker && $_
-downloadJaidScript installGithubReleaseWriter && $_
+# downloadJaidScript installFfmpeg && $_
+# downloadJaidScript installYoutubeDl && $_
+# downloadJaidScript installDocker && $_
+# downloadJaidScript installGithubReleaseWriter && $_
 
-rm ~/.bashrc
-rm ~/.bash_logout
-rm ~/.profile
-rm ~/setup.bash
+# rm ~/.bashrc
+# rm ~/.bash_logout
+# rm ~/.profile
+# rm ~/setup.bash
