5846cc074b083855a7638f221314b28c8ff41620
Jaid <jaid.jsx@gmail.com>
2022-05-26 18:58:47 +0200

    Improved zsh
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- commands/installZsh.bash
+++ commands/installZsh.bash
@@ -16,12 +16,15 @@ gitClone robbyrussell/oh-my-zsh
 
 chmod --recursive go-w $ZSH
 
+touch ~/.secrets.sh
+
 echo "export ZSH=$ZSH
 export ZSH_THEME=agnoster
 export LANG=en_US.UTF-8
-export PATH=\$PATH:$HOME/.yarn/bin:$USER_BIN_DIR
+export PATH=\$PATH:$USER_BIN_DIR
 export MAKEFLAGS=-j$(nproc)
 export NODE_OPTIONS=--max_old_space_size=8000
+
 export LANG=en_US.UTF-8
 export PERSONAL_LOCALE=de_DE.UTF-8
 export LANGUAGE=\$LANG
@@ -36,16 +39,29 @@ export LC_CTYPE=\$PERSONAL_LOCALE
 export LC_NAME=\$PERSONAL_LOCALE
 export LC_MESSAGES=\$LANG
 export LC_TIME=\$PERSONAL_LOCALE
+
 export EDITOR=nano
+
+ZSH_COMMAND_TIME_MIN_SECONDS=5
+ZSH_COMMAND_TIME_EXCLUDE=(
+  nano
+)
+
 plugins=(
 )
+
 source $ZSH/oh-my-zsh.sh
+
 setopt histreduceblanks
 setopt histignorealldups
 setopt globdots
 setopt extendedglob
 setopt listpacked
-setopt rmstarsilent" | dd of=~/.zshrc
+setopt rmstarsilent
+
+[ -f ~/.secrets ] && source ~/.secrets
+[ -f ~/.secrets.sh ] && source ~/.secrets.sh
+" | dd of=~/.zshrc
 
 downloadZshPlugin zsh-autosuggestions zsh-users/zsh-autosuggestions
 downloadZshPlugin zsh-syntax-highlighting zsh-users/zsh-syntax-highlighting
@@ -56,3 +72,4 @@ enableZshPlugin git
 enableZshPlugin httpie
 enableZshPlugin sudo
 enableZshPlugin zbell
+downloadZshPlugin zsh-command-time popstas/zsh-command-time
