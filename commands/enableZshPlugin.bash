e88b321c84ce8b1618426b510e97fd02389913ff
Jaid <jaid.jsx@gmail.com>
2022-06-23 20:20:48 +0200

    Update enableZshPlugin.bash

--- commands/enableZshPlugin.bash
+++ commands/enableZshPlugin.bash
@@ -4,6 +4,6 @@ set -o errexit
 
 ZSH_PLUGINS=$ZSH/custom/plugins
 
-sed --zero-terminated --in-place "s/plugins=(\n/plugins=(\n  $1\n/" ~/.zshrc
+sed --zero-terminated --in-place "s|plugins=(\n|plugins=(\n  $1\n|" ~/.zshrc
 
 echo "Enabled zsh plugin: $1"
