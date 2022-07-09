e225f3b4e93a2af6ec5868689ed9f15671200876
Jaid <jaid.jsx@gmail.com>
2022-07-09 12:24:10 +0200

    Improved installNode
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- commands/installNode.bash
+++ commands/installNode.bash
@@ -4,7 +4,7 @@ set -o errexit
 
 aptGet install build-essential
 
-curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+curl -fsSL https://deb.nodesource.com/setup_current.x | bash -
 aptGet install nodejs
 
 sudo npm install --global npm replace rimraf json5 getfilesize-cli pkg bin-version-cli find-by-extension-cli package-name-cli package-field-cli jaid-logger-tail degit
