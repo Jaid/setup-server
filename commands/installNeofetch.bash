ac63bd2a1b503380125addbec0054b6706ef4d70
Jaid <jaid.jsx@gmail.com>
2022-07-09 11:56:39 +0200

    Improved neofetch installation
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- commands/installNeofetch.bash
+++ commands/installNeofetch.bash
@@ -3,4 +3,7 @@ set -e
 set -o errexit
 
 gitClone dylanaraps/neofetch
-echo -e "\nbash ~/src/neofetch/neofetch" | tee -a ~/.zshrc
+
+echo -e "\nnerd_font=on bash ~/src/neofetch/neofetch --title_fqdn on --kernel_shorthand on --memory_percent on --memory_unit gib --cpu_temp C --underline_char  --kernel_shorthand off --block_width 4 --memory_display on --cpu_cores physical --speed_shorthand on" | tee -a ~/.zshrc
+
+downloadJaidFile neofetch.conf ~/.config/neofetch/config.conf
