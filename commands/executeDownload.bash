f3b75ed0796ce879c6ffb0cccc5af9be7d35d935
Jaid <jaid.jsx@gmail.com>
2022-07-09 13:35:25 +0200

    Improved executeDownload
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- commands/executeDownload.bash
+++ commands/executeDownload.bash
@@ -6,5 +6,4 @@ mkdir -p ~/.cache/executeDownload
 hash=$(echo -n "$1" | md5sum | awk '{print $1}')
 downloadPath=~/.cache/executeDownload/$hash.bash
 downloadFile "$1" "$downloadPath"
-chmod +x "$downloadPath"
-"$downloadPath"
+bash "$downloadPath"
