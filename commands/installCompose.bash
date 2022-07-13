e27f91774548fdab8922cc60183f7f789c4184f5
Jaid <jaid.jsx@gmail.com>
2022-07-13 12:18:12 +0200

    Improved installCompose
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- commands/installCompose.bash
+++ commands/installCompose.bash
@@ -3,6 +3,11 @@ set -e
 set -o errexit
 
 if [ ! -x "$(command -v docker)" ]; then
+  echo "Need command degit"
+  exit 1
+fi
+
+if [ ! -x "$(command -v docker)" ]; then
   echo "No docker command, installing"
   if [ ! -x "$(command -v installDocker)" ]; then
     containers_healthy="Also no installDocker command"
@@ -13,8 +18,10 @@ if [ ! -x "$(command -v docker)" ]; then
 fi
 
 projectFolder="$HOME/docker/$1"
+composeFile="$projectFolder/docker-compose.yml"
 
 if [ -d "$projectFolder" ]; then
+  docker compose --file "$composeFile" down --remove-orphans --timeout 30 --rmi all
   backupFolder="$HOME/docker-project-$1-old"
   echo "Project already exists, moving to $backupFolder"
   mv "$projectFolder" "$backupFolder"
@@ -23,10 +30,10 @@ else
 fi
 
 degit "Jaid/setup-server/composes/$1" "$projectFolder"
-docker compose --file "$projectFolder/docker-compose.yml" up --no-start
+docker compose --file "$composeFile" up --no-start
 
 echo "Added docker-compose project: $1"
 echo "Start with:"
-echo "docker compose --file \"\$HOME/docker/$1/docker-compose.yml\" up --detach"
+echo "docker compose --file '$composeFile' up --detach"
 echo "Restart with:"
-echo "docker compose --file \"\$HOME/docker/$1/docker-compose.yml\" down --remove-orphans --timeout 30 && docker compose --file \"\$HOME/docker/$1/docker-compose.yml\" up --detach"
+echo "docker compose --file '$composeFile' down --remove-orphans --timeout 30 && docker compose --file '$composeFile' up --detach"
