f79d9227bfd36bebabbfa0af85a4ad634edb8d26
Jaid <jaid.jsx@gmail.com>
2022-07-13 13:26:02 +0200

    Added deleteCompose
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

---
+++ commands/deleteCompose.bash
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -e
+set -o errexit
+
+dockerFolder="$HOME/docker"
+projectName=$1
+projectFolder="$dockerFolder/$projectName"
+composeFile="$projectFolder/docker-compose.yml"
+
+echo "Stopping compose: $1"
+docker compose --file "$composeFile" down --remove-orphans --timeout 30
+docker compose --file "$composeFile" rm --volumes --force
+
+echo "Deleting compose: $1"
+sudo rm -rf "$projectFolder"
