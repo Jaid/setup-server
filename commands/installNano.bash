bfa5cd791036e5429ce2bc1ab8a852c013e49764
Jaid <jaid.jsx@gmail.com>
2022-07-16 01:57:52 +0200

    Added installNano
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

---
+++ commands/installNano.bash
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+set -e
+set -o errexit
+
+configFolder="$HOME/.config/nano"
+rcFile="$configFolder/nanorc"
+repo=scopatz/nanorc
+repoFolder="$HOME/src/$repo"
+
+mkdir --parents "$configFolder"
+aptGet install nano
+
+if [ ! -d "$repoFolder" ]; then
+  echo "$repoFolder does not exist, cloning"
+  gitClone $repo
+else
+  git -C "$repoFolder" pull --ff-only --quiet
+fi
+
+addSyntax() {
+  printf 'include "%s/%s.nanorc"\n' "$repoFolder" "$1" >>"$rcFile"
+}
+
+addSyntax c
+addSyntax conf
+addSyntax css
+addSyntax html
+addSyntax ini
+addSyntax js
+addSyntax json
+addSyntax markdown
+addSyntax properties
+addSyntax sh
+addSyntax sql
+addSyntax systemd
+addSyntax ts
+addSyntax yaml
+addSyntax zsh
