2dbad208946685f27b24775487892e2b90377646
Jaid <jaid.jsx@gmail.com>
2022-07-15 05:29:05 +0200

    Added script downloadJaidScripts
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

---
+++ commands/downloadJaidScripts.bash
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+set -o errexit
+set +o monitor
+shopt -s lastpipe
+
+if [ ! -x "$(command -v gitClone)" ]; then
+  echo "Need command gitClone"
+  exit 1
+fi
+
+repoFolder="$HOME/src/setup-server"
+inputFolder="$repoFolder/commands"
+outputFolder="$HOME/bin"
+
+if [ ! -d "$repoFolder" ]; then
+  echo "$repoFolder does not exist, cloning"
+  gitClone Jaid/setup-server
+fi
+
+cd "$repoFolder"
+git pull --ff-only --quiet
+
+declare -i allCount=0
+declare -i changedCount=0
+
+find "$inputFolder" -type f -printf '%f\n' | while read -r file; do
+  scriptName=$(printf %s "$file" | sed 's|\.\w\+$||')
+  inputFile="$inputFolder/$file"
+  outputFile="$outputFolder/$scriptName"
+  if [ -f "$outputFile" ]; then
+    inputMd5=$(md5sum "$inputFile" | cut --fields 1 --delimiter " ")
+    outputMd5=$(md5sum "$outputFile" | cut --fields 1 --delimiter " ")
+    if [ "$inputMd5" != "$outputMd5" ]; then
+      echo "Updating: $scriptName"
+      cp --no-preserve=mode,ownership "$inputFile" "$outputFile"
+      changedCount=$((changedCount++))
+    fi
+  else
+    echo "Adding: $scriptName"
+    cp "$inputFile" "$outputFile"
+    changedCount=$((changedCount++))
+  fi
+  allcount=$((allCount++))
+done
+
+printf 'Updated %s/%s scripts\n' "$changedCount" "$allCount"
