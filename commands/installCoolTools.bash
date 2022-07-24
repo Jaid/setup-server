b1a06fd0badec8a6f81f7299492193e7fcd4ab62
Jaid <jaid.jsx@gmail.com>
2022-07-24 20:00:34 +0200

    Improved logic of installCoolTools
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- commands/installCoolTools.bash
+++ commands/installCoolTools.bash
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 set -o errexit
 
-aptGet update
-aptGet install ncdu nethogs htop aria2 zip unzip exa bonnie++ fd-find
+declare -a missingAptPackages=()
+declare -a requiredAptPackages=(install ncdu nethogs htop aria2 zip unzip exa bonnie++ fd-find)
+for requiredAptPackage in "${requiredAptPackages[@]}"; do
+  if [ ! "$(dpkg -l "$requiredAptPackage")" ]; then
+    missingAptPackages+=("$requiredAptPackage")
+  fi
+done
+if [ ${#missingAptPackages[@]} -gt 0 ]; then
+  printf 'Installing apt packages: %s' "${missingAptPackages[@]}"
+  aptGet update
+  aptGet install "${missingAptPackages[@]}"
+fi
