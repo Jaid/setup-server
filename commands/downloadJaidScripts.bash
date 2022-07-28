de8db8e17c9db63c0e4722e0d16758e8438c7a3a
Jaid <jaid.jsx@gmail.com>
2022-07-28 05:21:15 +0200

    Added new script
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- commands/downloadJaidScripts.bash
+++ commands/downloadJaidScripts.bash
@@ -6,42 +6,56 @@ shopt -s lastpipe
 : "${otherReposFolder:="$HOME/src"}"
 : "${userBinFolder:="$HOME/bin"}"
 
-repoFolder="$otherReposFolder/setup-server"
 inputFolder="$repoFolder/commands"
 
 mkdir --parents "$userBinFolder"
+mkdir --parents "$otherReposFolder"
 
-if [ ! -d "$repoFolder" ]; then
-  echo "$repoFolder does not exist, cloning"
-  mkdir --parents "$otherReposFolder"
-  git clone https://github.com/Jaid/setup-server.git "$repoFolder"
-  git checkout dist
-else
-  git -C "$repoFolder" pull --ff-only --quiet
-fi
-
-declare -i allCount=0
-declare -i changedCount=0
-
-find "$inputFolder" -type f -printf '%f\n' | while read -r file; do
-  scriptName=$(printf %s "$file" | sed 's|\.\w\+$||')
-  inputFile="$inputFolder/$file"
-  outputFile="$userBinFolder/$scriptName"
-  if [ -f "$outputFile" ]; then
-    inputMd5=$(md5sum "$inputFile" | cut --fields 1 --delimiter " ")
-    outputMd5=$(md5sum "$outputFile" | cut --fields 1 --delimiter " ")
-    if [ "$inputMd5" != "$outputMd5" ]; then
-      echo "Updating: $scriptName"
-      cp --no-preserve=mode,ownership "$inputFile" "$outputFile"
-      ((changedCount++)) || true
-    fi
+declare -a scriptRepos=(
+  setup-server
+  scripts
+)
+for scriptRepo in "${scriptRepos[@]}"; do
+  printf 'Repo: “%s”\n' "$scriptRepo"
+  repoFolder="$otherReposFolder/$scriptRepo"
+  if [ ! -d "$repoFolder" ]; then
+    printf "%s does not exist, cloning\n" "$repoFolder"
+    git clone https://github.com/Jaid/setup-server.git "$repoFolder"
+    git checkout dist
   else
-    echo "Adding: $scriptName"
-    cp "$inputFile" "$outputFile"
-    chmod +x "$inputFile"
-    ((changedCount++)) || true
+    git -C "$repoFolder" pull --ff-only --quiet
   fi
-  ((allCount++)) || true
+  repoBinFolder="$repoFolder/bin"
+  find "$repoBinFolder" -type f -printf '%f\0' | while read -r -d $'\0' file; do
+    fullPath="$repoBinFolder/$file"
+  done
+  if [ -x "$file" ]; then
+    :
+  fi
 done
+
+# declare -i allCount=0
+# declare -i changedCount=0
+
+# find "$inputFolder" -type f -printf '%f\n' | while read -r file; do
+#   scriptName=$(printf %s "$file" | sed 's|\.\w\+$||')
+#   inputFile="$inputFolder/$file"
+#   outputFile="$userBinFolder/$scriptName"
+#   if [ -f "$outputFile" ]; then
+#     inputMd5=$(md5sum "$inputFile" | cut --fields 1 --delimiter " ")
+#     outputMd5=$(md5sum "$outputFile" | cut --fields 1 --delimiter " ")
+#     if [ "$inputMd5" != "$outputMd5" ]; then
+#       echo "Updating: $scriptName"
+#       cp --no-preserve=mode,ownership "$inputFile" "$outputFile"
+#       ((changedCount++)) || true
+#     fi
+#   else
+#     echo "Adding: $scriptName"
+#     cp "$inputFile" "$outputFile"
+#     chmod +x "$inputFile"
+#     ((changedCount++)) || true
+#   fi
+#   ((allCount++)) || true
+# done
 
 printf 'Updated %s/%s scripts\n' "$changedCount" "$allCount"
