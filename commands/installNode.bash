0f057cab9e9fd7088f7b18ddbe5afc35d8cbdf8e
Jaid <jaid.jsx@gmail.com>
2022-07-28 14:07:22 +0200

    Improved installNode stability
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- commands/installNode.bash
+++ commands/installNode.bash
@@ -3,6 +3,8 @@ set -o errexit
 
 curl -fsSL https://deb.nodesource.com/setup_current.x | sudo -E bash -
 requireAptPackages gcc g++ make
+aptGet clean
+aptGet autoremove
 aptGet install nodejs
 
 sudo npm install --global npm replace rimraf json5 getfilesize-cli pkg bin-version-cli find-by-extension-cli package-name-cli package-field-cli jaid-logger-tail degit
