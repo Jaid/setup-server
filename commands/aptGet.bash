7aeb95331262a7a45dae656c1a1ce9eaebbab0d0
Jaid <jaid.jsx@gmail.com>
2022-07-28 12:36:19 +0200

    Moved a bunch of scripts to https://github.com/jaid/scripts
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- commands/aptGet.bash
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-set -o errexit
-
-DEBIAN_FRONTEND=noninteractive
-sudo apt-get --option Acquire::Retries=60 --option Acquire::http::Timeout=180 --yes --quiet "$@"
--- commands/executeDownload.bash
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -o errexit
-
-mkdir -p ~/.cache/executeDownload
-hash=$(echo -n "$1" | md5sum | awk '{print $1}')
-downloadPath=~/.cache/executeDownload/$hash.bash
-downloadFile "$1" "$downloadPath"
-bash "$downloadPath"
