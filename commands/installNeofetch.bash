7100d7e875faaa7f26e58fd0f11c3b5cf288b8bf
Jaid <jaid.jsx@gmail.com>
2022-07-29 13:02:02 +0200

    Migrated all files to §_install
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- commands/installNeofetch.bash
+++ commands/installNeofetch.bash
@@ -5,4 +5,4 @@ gitClone dylanaraps/neofetch
 
 echo -e "\nnerd_font=on bash ~/src/neofetch/neofetch --title_fqdn on --kernel_shorthand on --memory_percent on --memory_unit gib --cpu_temp C --underline_char  --kernel_shorthand off --block_width 4 --memory_display on --cpu_cores physical --speed_shorthand on" | tee -a ~/.zshrc
 
-downloadJaidFile neofetch.conf ~/.config/neofetch/config.conf
+installGist ba4ebe46f1dea42972b912747feb9365
--- commands/setupLanguage.bash
+++ commands/setupLanguage.bash
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -o errexit
 
-downloadJaidFile locale.gen /etc/locale.gen
+installGist eac9757fdb899067a64f5880524823d9
 sudo locale-gen --purge
