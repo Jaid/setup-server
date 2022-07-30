3f5521415f0bb8ec9e0fe7457bb3824b3de08d86
Jaid <jaid.jsx@gmail.com>
2022-07-30 16:56:19 +0200

    Improved installGh
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- commands/installFfmpeg.bash
+++ commands/installFfmpeg.bash
@@ -4,23 +4,24 @@ set -o errexit
 : "${otherReposFolder:="$HOME/src"}"
 : "${userBinFolder:="$HOME/bin"}"
 
-mkdir --parents $SRC_DIR/ffmpeg-build
+buildFolder="$otherReposFolder/ffmpeg-build"
+mkdir --parents "$buildFolder"
 
 gitClone mstorsjo/fdk-aac
-cd $SRC_DIR/fdk-aac
+cd "$otherReposFolder/fdk-aac"
 ./autogen.sh
-./configure --prefix="$SRC_DIR/ffmpeg-build"
+./configure --prefix="$buildFolder"
 make
 make install
 
-gitClone videolan/x265
-cd $SRC_DIR/x265/build/linux
-cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$SRC_DIR/ffmpeg-build" -DENABLE_SHARED=off ../../source
-make
-make install
+# gitClone videolan/x265
+# cd $otherReposFolder/x265/build/linux
+# cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$otherReposFolder/ffmpeg-build" -DENABLE_SHARED=off ../../source
+# make
+# make install
 
-gitClone FFmpeg/FFmpeg latest $SRC_DIR/ffmpeg
-cd $_
-PKG_CONFIG_PATH="$SRC_DIR/ffmpeg-build/lib/pkgconfig" ./configure --prefix=$SRC_DIR --bindir="$USER_BIN_DIR" --pkg-config-flags="--static" --extra-cflags="-march=native -I$SRC_DIR/ffmpeg-build/include" --extra-ldflags="-L$SRC_DIR/ffmpeg-build/lib" --extra-libs="-lpthread -lm" --enable-hardcoded-tables --enable-gpl --enable-libass --enable-libfreetype --enable-libmp3lame --enable-libopus --enable-libvorbis --enable-libvpx --enable-libx264 --enable-libx265 --enable-nonfree
-make
-make install
+# gitClone FFmpeg/FFmpeg latest $otherReposFolder/ffmpeg
+# cd $_
+# PKG_CONFIG_PATH="$otherReposFolder/ffmpeg-build/lib/pkgconfig" ./configure --prefix=$otherReposFolder --bindir="$USER_BIN_DIR" --pkg-config-flags="--static" --extra-cflags="-march=native -I$otherReposFolder/ffmpeg-build/include" --extra-ldflags="-L$otherReposFolder/ffmpeg-build/lib" --extra-libs="-lpthread -lm" --enable-hardcoded-tables --enable-gpl --enable-libass --enable-libfreetype --enable-libmp3lame --enable-libopus --enable-libvorbis --enable-libvpx --enable-libx264 --enable-libx265 --enable-nonfree
+# make
+# make install
