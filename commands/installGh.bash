b18b837144e9187ffa1138c199c2692d734465cc
Jaid <jaid.jsx@gmail.com>
2022-07-30 06:23:30 +0200

    Added script installGh
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

---
+++ commands/installGh.bash
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -o errexit
+
+# From https://github.com/cli/cli/blob/trunk/docs/install_linux.md#debian-ubuntu-linux-raspberry-pi-os-apt
+
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list >/dev/null
+
+aptGet update
+aptGet install gh
