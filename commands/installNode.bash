4279b214103c0b7ae0e187ed70512e44d98c602d
Jaid <jaid.jsx@gmail.com>
2022-08-02 18:22:53 +0200

    Improved installNode
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- commands/installNode.bash
+++ commands/installNode.bash
@@ -2,7 +2,7 @@
 set -o errexit
 
 curl -fsSL https://deb.nodesource.com/setup_current.x | sudo -E bash -
-requireAptPackages build-essential pkg-config gcc g++ make
+requireAptPackages build-essential pkg-config gcc g++ make python3
 aptGet clean
 aptGet autoremove
 aptGet install nodejs
