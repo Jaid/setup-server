5d9feebf5991cc1e506d64f0772d1769d70261aa
Jaid <jaid.jsx@gmail.com>
2022-09-11 07:46:05 +0200

    Moved scripts to https://github.com/Jaid/scripts
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- commands/installNeofetch.bash
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -o errexit
-
-gitClone dylanaraps/neofetch
-
-# echo -e "\nnerd_font=on bash ~/src/neofetch/neofetch --title_fqdn on --kernel_shorthand on --memory_percent on --memory_unit gib --cpu_temp C --underline_char  --kernel_shorthand off --block_width 4 --memory_display on --cpu_cores physical --speed_shorthand on" | tee -a ~/.zshrc
-
-installGist ba4ebe46f1dea42972b912747feb9365
--- commands/installSshServer.bash
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -o errexit
-
-requireAptPackages openssh-server
-installGist e4673390a04dd0ce2fe4dc4c4c7ed592
-sudo systemctl enable ssh
-sudo service ssh start
