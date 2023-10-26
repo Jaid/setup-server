5fa20f092eee2fed5ff797e14024802429f9964e
Jaid <6216144+Jaid@users.noreply.github.com>
2023-10-26 11:06:16 +0200

    Updated scrutiny
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- createHetznerServer.bash
+++ createHetznerServer.bash
@@ -1,8 +1,19 @@
+name=${name:-test}
+location=${location:-nbg1}
+type=${type:-cax11}
+sshKey=${sshKey:-vps}
+ubuntuVersion=${ubuntuVersion:-22.04}
+
+if [[ -z "$hetznerToken" ]]; then
+  echo "Please set hetznerToken"
+  exit 1
+fi
+
 cloudConfig=$(<cloudconfig.yml)
 cloudConfig=$(sed -z 's/\\/\\\\/g' <<<"$cloudConfig")
 cloudConfig=$(sed -z 's/\"/\\\"/g' <<<"$cloudConfig")
 cloudConfig=$(sed -z 's/\n/\\n/g' <<<"$cloudConfig")
-json="{\"automount\":false,\"image\":\"ubuntu-22.04\",\"location\":\"nbg1\",\"name\":\"test\",\"server_type\":\"cx21\",\"ssh_keys\":[\"jaid.jsx+hub@gmail.com\"],\"start_after_create\":true,\"user_data\":\"$cloudConfig\"}"
+json="{\"automount\":false,\"image\":\"ubuntu-$ubuntuVersion\",\"location\":\"$location\",\"name\":\"$name\",\"server_type\":\"$type\",\"ssh_keys\":[\"$sshKey\"],\"start_after_create\":true,\"user_data\":\"$cloudConfig\"}"
 curl -X POST -H "Authorization: Bearer $hetznerToken" -H "Content-Type: application/json" -d "$json" 'https://api.hetzner.cloud/v1/servers'
 
 # curl -H "Authorization: Bearer $hetznerToken" 'https://api.hetzner.cloud/v1/ssh_keys'
